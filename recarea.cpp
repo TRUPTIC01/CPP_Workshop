92b944394bd7331233fca34b43a82af6861e160a
TRUPTIC01 <truptichinchole1@gmail.com>
2021-05-27 00:07:45 +0530

    Area of rectangle

---
+++ recarea.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int length,breadth,area;
+        cout<<"Enter length and breadth of rectangle"<<endl
+;        cin>>length>>breadth;
+        area=length*breadth;
+        cout<<"Area of Rectangle is:"<<area<<endl;
+        return 0;
+}
