041706a526c35a27ad5a65f354781bf8e6afd97d
TRUPTIC01 <truptichinchole1@gmail.com>
2021-05-27 00:12:06 +0530

    Equivalent Character

---
+++ asciich.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        char eqCh='d'+3;
+        cout<<eqCh;    //Equivalent character                                    
+        return 0;
+}
+
